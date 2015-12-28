bf492c2f8fe239c8f1ee23a2d1b960999122802b
Juri Kern <juri@sent.at>
2015-12-28 19:33:10 +0200

    manage window

--- GameTutorial/main.cpp
+++ GameTutorial/main.cpp
@@ -1,17 +1,10 @@
-#include <iostream>
-
-#include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
-
 #include "window.h"
-
-using namespace std;
 
 int main()
 {
-    Window* window = new Window;
-    
-    cout << "Started" << endl;
+    Window* window = new Window("Game 640x480", 680, 480);
+    window->Init();
+    window->Loop();
     
     delete window;
     
--- GameTutorial/window.cpp
+++ GameTutorial/window.cpp
@@ -1,8 +1,63 @@
 #include "window.h"
 
-Window::Window() : mWidth(0), mHeight(0) {}
+Window::Window(string n, int w, int h) : mName(n), mWidth(w), mHeight(h), mWindow(nullptr) {}
 
-Window::~Window() {}
+Window::~Window()
+{
+    SDL_DestroyRenderer(mRenderer);
+    SDL_DestroyWindow(mWindow);
+    IMG_Quit();
+    SDL_Quit();
+}
+
+bool Window::Init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+        return false;
+    
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
+    mWindow = SDL_CreateWindow(GetName().c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, GetWidth(), GetHeight(), SDL_WINDOW_SHOWN);
+    
+    if (mWindow == nullptr)
+        return false;
+    
+    mRenderer = SDL_CreateRenderer(mWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    
+    if (mRenderer == nullptr)
+        return false;
+    
+    SDL_SetRenderDrawColor(mRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    
+    int imgFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgFlags) & imgFlags))
+        return false;
+    
+    return true;
+}
+
+void Window::Loop()
+{
+    bool quit = false;
+    SDL_Event e;
+    
+    while (!quit)
+    {
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+                quit = true;
+        }
+        
+        SDL_SetRenderDrawColor(mRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(mRenderer);
+        SDL_RenderPresent(mRenderer);
+    }
+}
+
+string Window::GetName() const
+{
+    return mName;
+}
 
 int Window::GetWidth() const
 {
