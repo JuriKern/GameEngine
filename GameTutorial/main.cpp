f472cdbf8fa832f22798b1e7fb6d60c5136c28c4
Juri Kern <juri@sent.at>
2015-12-28 18:00:56 +0200

    Initial Commit

---
+++ GameTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameTutorial
+//
+//  Created by Juri on 28/12/15.
+//  Copyright © 2015 Tfy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
